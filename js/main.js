2dcbdeb469f19161780861501d450e2111967265
Narendra Jethi <narendrajethi220@gmail.com>
2023-01-29 16:34:44 +0530

    docs- home page
    This is the home page of our E-learning Management system name Coding Hour. It is made up of using HTML,CSS and JavaScript however it is not responsive

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//Navbar styling
+
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
+
+// show/hide faq
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+    // change icon
+    const icon = faq.querySelector(".faq_icon i");
+    if (icon.className === "uil uil-plus") {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus";
+    }
+  });
+});
